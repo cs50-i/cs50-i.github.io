6dd1847a78bc128c78703287cc5f239e800da813
cs50-i <69306336+cs50-i@users.noreply.github.com>
2022-09-20 23:43:56 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,32 @@
+
+const heart=document.getElementById("heart");
+
+
+heart.addEventListener("click",defpage)
+
+
+    let c="#1178ed"
+    function changec(c){
+        c=prompt("what's ur fav color?")
+        but.style.color=c
+    }
+
+    function h(){
+    
+        but.style.color="white";
+    }
+
+
+function al(){
+    x=prompt("Hello\n what is ur name?");
+    if (x==null){
+        return 0;
+    }
+
+    but.innerText="Hi"+" "+x
+    alert("hey "+x);
+}
+
+function defpage(){
+    location.href="default.html"
+}
